27c8ae2d0f1e8071df065183b589652e716f137a
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-16 20:34:29 +0300

    Created GlobalStyle.jsx and linked In the App.jsx.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
 import user from './Profile/user.json';
 import { UserProfile } from './Profile/Profile';
 
+import data from './Statistics/data.json';
+import { Statistics } from './Statistics/Statistics';
+
+import friends from './Friends/friends.json';
+import { FriendList } from './Friends/FriendList/FriendList';
+
+import transactions from './TransactionHistory/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import { GlobalStyle } from '../GlobalStyle';
+
 export const App = () => {
   return (
     <div
@@ -20,7 +31,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+      <GlobalStyle />
+ 
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { StylesProvider } from 'styled-components';
+import { styles } from './styles';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <StylesProvider styles={styles}>
+      <App />
+    </StylesProvider>
+  </React.StrictMode>
 );
